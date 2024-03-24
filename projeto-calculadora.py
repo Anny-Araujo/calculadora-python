ce943dd4c46be5213fa47225af774dd3c750acb4
Anny Araujo <105942075+Anny-Araujo@users.noreply.github.com>
2024-03-24 18:40:30 -0300

    projeto-calculadora

---
+++ projeto-calculadora.py
@@ -0,0 +1,46 @@
+print("Vamos calcular?")
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if num1 >= num2:
+  resultado = num1 * num2
+  print("Aqui esta sua multiplicação:", resultado)
+elif num1 <= num2:
+  result = num1 * num2
+  print("Aqui esta sua multiplicação:", result)
+else:
+  print("Seu resultado é:", resultado)
+
+num3 = int(input("Digite um número: "))
+num4 = int(input("Digite outro número: "))
+
+if num3 >= num4:
+  resultado1 = num3 + num4
+  print("Aqui esta sua soma:", resultado1)
+elif num3 <= num4:
+  result1 = num3 + num4
+  print("Aqui esta sua soma:", result1)
+else:
+  print("Seu resultado é:", resultado1)
+
+num5 = int(input("Digite um número: "))
+num6 = int(input("Digite outro número: "))
+if num5 >= num6:
+  resultado2 = num5 / num6
+  print("Aqui esta sua divisão:", resultado2)
+elif num5 <= num6:
+  resul2t = num5 / num6
+  print("Aqui esta sua multiplicação:", result2)
+else:
+  print("Seu resultado é:", resultado2)
+
+num7 = int(input("Digite um número: "))
+num8 = int(input("Digite outro número: "))
+if num5 >= num6:
+  resultado3 = num7 - num8
+  print("Aqui esta sua subtração:", resultado3)
+elif num7 <= num8:
+  result3 = num7 * num8
+  print("Aqui esta sua multiplicação:", result3)
+else:
+  print("Seu resultado é:", resultado3)
